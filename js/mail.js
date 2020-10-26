86b001f94dd15b3a8fd9e3caf667eff356ac708a
stanislav-sn <stas,snezhkovoy@gmail.com>
2020-10-26 14:35:34 +0200

    upload site

---
+++ js/mail.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#carousel-header").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        loop: true,
+        navText: []
+        
+    });
+  });
